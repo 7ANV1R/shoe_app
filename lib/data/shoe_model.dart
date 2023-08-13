239e2b70499c99034a338122fa4f28a25be1e47d
Tanvir <ibnmizan7878@gmail.com>
2023-08-14 01:21:15 +0600

    updated shoe app

--- lib/data/shoe_model.dart
+++ lib/data/shoe_model.dart
@@ -4,11 +4,13 @@ class Shoe {
     required this.price,
     required this.img,
     required this.prodPrimaryColor,
+    required this.description,
   });
   String name;
   double price;
   String img;
   String prodPrimaryColor;
+  String description;
 
   static List<Shoe> productList = [
     Shoe(
@@ -16,24 +18,32 @@ class Shoe {
       price: 125,
       img: 'assets/shoe/shoe0.png',
       prodPrimaryColor: 'be0000',
+      description:
+          "Created for the hardwood but taken to the streets, the b-ball icon returns with crisp synthetic leather and retro colors. The classic design channels '80s vintage back onto the streets while the padded, high-top collar adds an old-school look rooted to comfort.",
     ),
     Shoe(
       name: 'Air Jordan 1 Retro High OG',
       price: 180,
       img: 'assets/shoe/shoe1.png',
       prodPrimaryColor: '005891',
+      description:
+          "Game on! Inspired by MJ's high school days, this energetic colorway brings A+ energy to the streets. Bold bursts of Game Royal and Varsity Maize let you join the team in style, while subtly weathered leather ups the vintage appeal. Crafted details like the woven tongue label, hangtag and Wings logo add a premium boost to any 'fit. And the finishing touch? Nike Air cushioning underfoot lets you keep pace, so nothing holds you back from showing off your legendary look.",
     ),
     Shoe(
       name: 'Air Jordan 1 Mid SE',
       price: 135,
       img: 'assets/shoe/shoe2.png',
       prodPrimaryColor: '673335',
+      description:
+          "Dope sneakers count as self-care. And with colors inspired by the aisles of your neighborhood beauty supply store (plus cloud-like Nike Air cushioning underfoot), these mid-top J's will have you feeling anything but mid. Go ahead—treat yourself.",
     ),
     Shoe(
       name: 'Nike Dunk High Retro',
       price: 135,
       img: 'assets/shoe/shoe3.png',
       prodPrimaryColor: 'eb4835',
+      description:
+          "Created for the hardwood but taken to the streets, the '80s b-ball icon returns with perfectly sheened overlays and original university colors. With its classic hoops design, the Nike Dunk High Retro channels '80s vintage back onto the streets while its padded, high-top collar adds an old-school look rooted in comfort.",
     ),
   ];
 }
--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'dart:math' as math;
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shoe_app/const/ui_helper.dart';
 import 'package:shoe_app/data/shoe_model.dart';
 
 import '../const/asset_const.dart';
+import '../local_package/slide_to_go.dart';
+import '../util/hex_color.dart';
 
 class DetailsPage extends StatefulHookConsumerWidget {
   const DetailsPage({super.key, required this.shoe});
@@ -32,59 +35,140 @@ class _DetailsPageState extends ConsumerState<DetailsPage> with SingleTickerProv
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: nikeAppBarWithBackButton(size),
-      body: SingleChildScrollView(
-          child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              item.name,
-              style: const TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              '\$${item.price.toStringAsFixed(0)}',
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Hero(
-              tag: item.img,
-              child: Transform.rotate(
-                angle: -35 * math.pi / 180,
-                child: Container(
-                  clipBehavior: Clip.none,
-                  child: AnimatedBuilder(
-                      animation: animationController,
-                      builder: (context, snapshot) {
-                        final valueAnimation = Curves.linear.transform(animationController.value);
-                        return Opacity(
-                          opacity: 1,
-                          child: Image.asset(
-                            item.img,
-                            height: 297.88 + 20,
-                            width: 297.88 + 20,
-                          )
-                              .animate()
-                              .scaleXY(
-                                end: 1,
-                              )
-                              .rotate(
-                                begin: 0 * valueAnimation,
-                                end: 0.09 * valueAnimation,
-                              ),
-                        );
-                      }),
+      // bottomNavigationBar: Padding(
+      //   padding: const EdgeInsets.all(16.0),
+      //   child: Container(
+      //     height: 70,
+      //     width: size.width,
+      //     decoration: BoxDecoration(
+      //       color: Colors.white,
+      //       borderRadius: BorderRadius.circular(12),
+      //       boxShadow: [
+      //         BoxShadow(
+      //           color: Colors.black.withOpacity(0.5),
+      //           blurRadius: 10,
+      //           offset: const Offset(0, -5),
+      //         ),
+      //       ],
+      //     ),
+      //     child: SlideAction(
+      //       onSubmit: () {},
+      //       sliderButtonIcon: const Icon(
+      //         Icons.shopping_cart,
+      //         color: Colors.white,
+      //       ),
+      //       text: 'Add to cart',
+      //       textStyle: const TextStyle(
+      //         fontSize: 18,
+      //         color: Colors.white,
+      //         fontWeight: FontWeight.w500,
+      //       ),
+      //       borderRadius: 12,
+      //       elevation: 0,
+      //       innerColor: HexColor(item.prodPrimaryColor),
+      //       outerColor: HexColor(item.prodPrimaryColor).withOpacity(0.5),
+      //     ),
+      //   ),
+      // ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        item.name,
+                        style: const TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '\$${item.price.toStringAsFixed(0)}',
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Hero(
+                        tag: item.img,
+                        child: Transform.rotate(
+                          angle: -35 * math.pi / 180,
+                          child: Container(
+                            clipBehavior: Clip.none,
+                            child: AnimatedBuilder(
+                                animation: animationController,
+                                builder: (context, snapshot) {
+                                  final valueAnimation = Curves.linear.transform(animationController.value);
+                                  return Opacity(
+                                    opacity: 1,
+                                    child: Image.asset(
+                                      item.img,
+                                      height: 297.88 + 20,
+                                      width: 297.88 + 20,
+                                    )
+                                        .animate()
+                                        .scaleXY(
+                                          end: 1,
+                                        )
+                                        .rotate(
+                                          begin: 0 * valueAnimation,
+                                          end: 0.09 * valueAnimation,
+                                        ),
+                                  );
+                                }),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        item.description,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      kVerticalSpaceXXXL,
+                      kVerticalSpaceXXXL,
+                    ],
+                  ),
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SizedBox(
+              height: 70,
+              width: size.width,
+              child: SlideAction(
+                onSubmit: () {},
+                sliderButtonIcon: const Icon(
+                  Icons.shopping_cart_checkout,
+                  color: Colors.white,
                 ),
-              ),
+                text: 'Add to cart',
+                textStyle: TextStyle(
+                  fontSize: 18,
+                  color: Colors.grey.shade700,
+                  fontWeight: FontWeight.w500,
+                ),
+                borderRadius: 12,
+                elevation: 0,
+                innerColor: HexColor(item.prodPrimaryColor),
+                outerColor: Colors.grey.shade300,
+              )
+                  .animate(
+                    onPlay: (controller) => controller.repeat(),
+                  )
+                  .shimmer(
+                    duration: const Duration(seconds: 4),
+                  ),
             ),
-          ],
-        ),
-      )),
+          ),
+        ],
+      ),
     );
   }
 
